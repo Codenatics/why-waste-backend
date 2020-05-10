cd63114914d8dd364406ca627a63ba4f2140269b
Sike Osinuga <adesikeoye@yahoo.com>
2020-05-10 01:18:24 +0100

    Get and POST API implemented

--- handler.js
+++ handler.js
@@ -1,18 +1,69 @@
-'use strict';
+// import express from "express";
+// React apps get Transpiled
+// This version of NodeJS does support import statements and there is no transpilation step
 
-module.exports.hello = async event => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
-        input: event,
-      },
-      null,
-      2
-    ),
-  };
+const serverlessHttp = require("serverless-http");
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const mysql = require("mysql");
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
-};
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: "WhatWaste",
+});
+
+// Logically separate 4 sections of code according to the method of the HTTP request received
+
+// Export a single function, called app
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+
+app.get("/restaurants/:PostCode", function (request, response) {
+  // const data = request.body;
+  const id = request.params.PostCode;
+  const query = `SELECT Name,FoodType,Quantity FROM Restaurants 
+  JOIN Food ON Restaurants.RestaurantId = Food.RestaurantId
+  WHERE PostCode LIKE ?`
+  // Should make a SELECT * FROM Restaurants query to the DB and return the results
+  connection.query(query, [id+'%'], function (err, data) {
+    if (err) {
+      console.log("Error from MySQL", err);
+      response.status(500).send(err);
+    } else {
+      response.status(200).send(data);
+    }
+  });
+});
+
+app.post("/restaurants", function (request, response) {
+  const data = request.body;
+  const query = `INSERT INTO Food (FoodType, Quantity, UseByDate, RestaurantID)
+   VALUES (?, ?, ?, (select RestaurantId from Restaurants where Name = ? ))`
+  // Should make a SELECT * FROM Restaurants query to the DB and return the results
+  connection.query(query, [data.FoodType, data.Quantity, data.UseByDate, data.Name], function (err, results) {
+    if (err) {
+      console.log("Error from MySQL", err);
+      response.status(500).send(err);
+    } else {
+      connection.query(
+        `SELECT * FROM Food WHERE FoodId = ${results.insertId}`,
+        function (err, results) {
+          if (err) {
+            console.log("Error from MySQL", err);
+            response.status(500).send(err);
+          } else {
+            response.status(201).send(results[0]);
+          }
+        }
+      );
+    }
+  }
+);
+});
+
+module.exports.app = serverlessHttp(app);
